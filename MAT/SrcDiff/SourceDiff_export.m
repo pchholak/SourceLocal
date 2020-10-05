0f6b259ada05ebb06aa17de3ab2f0661781ec156
pchholak <parthchholak@gmail.com>
2020-10-05 15:27:53 +0200

    finished coherence export

--- MAT/SrcDiff/SourceDiff_export.m
+++ MAT/SrcDiff/SourceDiff_export.m
@@ -1,9 +1,9 @@
 % The program should be started after clearing the workspace and exporting
-% the Test source time average file as 'xyz' and changing 'sub' and 'keywd'
+% the Test source time average file as 'xyz' and changing 'sub'
 
 %% Given
-sub = 2;
-info.res_path_coh = '/home/rommel/research/results/perception/Coherence';
+sub = 6;
+info.res_path_coh = '/home/rommel/research/results/perception/Coherence/';
 keywd = 'Coh(120by3s)';
 
 %% Locate and list .mat files
@@ -11,12 +11,13 @@ fmatF = dir([info.res_path_coh, keywd, '*_sub', sprintf('%02d',sub), '_F.mat']);
 fmatB = dir([info.res_path_coh, '*_sub', sprintf('%02d',sub), '_B.mat']);
 
 %% Load files, average and take difference
-load([path fmatF(1).name]); clear fmatF
+load([info.res_path_coh, fmatF(1).name]); clear fmatF
 srcm = mean(src, 2); srcF = [srcm, srcm];
-load([path fmatB(1).name]); clear fmatB
+load([info.res_path_coh, fmatB(1).name]); clear fmatB
 srcm = mean(src, 2); srcB = [srcm, srcm];
 img = srcF-srcB;
 
 %% Prepare to export
 xyz.ImageGridAmp = img;
 xyz.Comment = ['Diff_' keywd '_sub' sprintf('%02d',sub)];
+clear info
