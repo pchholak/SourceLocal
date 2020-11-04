1784d9d806836965696194e58df0f1987aa2905e
Parth Chholak <parthchholak@gmail.com>
2020-11-04 12:19:23 +0530

    spectral diff ready

---
+++ MAT/SpecDiff/SpecDiff_sub.m
@@ -0,0 +1,14 @@
+function P = SpecDiff_sub(info, keywd)
+
+% Locate and list .mat files
+fmatF = dir([info.res_path_power, '/', keywd, '*_' info.name '_F.mat']);
+fmatB = dir([info.res_path_power, '/', keywd, '*_' info.name '_B.mat']);
+
+% Load files, average and take difference
+load([info.res_path_power, '/', fmatF(1).name], 'src'); clear fmatF
+srcF = mean(src(info.scout_vert, 1, :), 3);
+load([info.res_path_power, '/', fmatB(1).name], 'src'); clear fmatB
+srcB = mean(src(info.scout_vert, 1, :),3);
+
+% Evaluate normalized difference of power (P)
+P = (srcF - srcB) / srcB;
